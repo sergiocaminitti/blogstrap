3812d210768cf62c723e205309ac3a31c7875bc8
SergioCaminitti <sergiocaminitti@gmail.com>
2025-02-13 15:22:28 -0300

    Article show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
-  #puxando todos os artigos do banco de dados
+    # puxando todos os artigos do banco de dados
   end
+  def show
+    @article = Article.find(params[:id])
+    # atribuindo para article o artigo que tem o id igual ao id passado na url
+  end
+
 end
