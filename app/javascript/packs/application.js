a044b62ee052699311209770b8862b72c5207cd6
SergioCaminitti <sergiocaminitti@gmail.com>
2025-02-14 16:06:12 -0300

    add function : 10 - Dividir para Conquistar 1/3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+require("@popperjs/core");
 import Rails from "@rails/ujs";
-import "bootstrap";
+import { Tooltip } from "bootstrap";
 import "../stylesheets/application";
-import "....../assets/stylesheets/application.scss";
-import "....../assets/stylesheets/bootstrap_custom.scss";
+
+document.addEventListener("turbolinks:load", () => {
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]',
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl),
+  );
+});
 
 Rails.start();
