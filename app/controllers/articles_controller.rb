e49bc36ada81c0801f9ac1d925284d90c9fdce99
SergioCaminitti <sergiocaminitti@gmail.com>
2025-02-14 11:10:02 -0300

    feature delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+  # executando antes de ações // only define antes de quais def's ele vai executar
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
     # puxando todos os artigos do banco de dados
   end
   def show
-    @article = Article.find(params[:id])
-    # atribuindo para article o artigo que tem o id igual ao id passado na url
   end
   def new
     @article = Article.new
@@ -21,19 +22,32 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    # puxando o artigo que tem o id igual ao id passado na url
-    @article = Article.find(params[:id])
   end
+  # def edit; end -- outra forma de escrever o método edit
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
       render :edit
     end
+
   end
+
+  def destroy
+    @article.destroy
+
+    redirect_to root_path
+  end
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+    # atribuindo para article o artigo que tem o id igual ao id passado na url
+  end
+
+
   # strong parameters para permitir que os parametros sejam passados para o model
   def article_params
     # definindo que os parametros permitidos são title e body
