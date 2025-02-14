898744431688e59614bf1a62fae226cd21300b6c
SergioCaminitti <sergiocaminitti@gmail.com>
2025-02-14 14:18:08 -0300

    add bootstrap

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import Rails from "@rails/ujs";
+import "bootstrap";
+import "../stylesheets/application";
+import "....../assets/stylesheets/application.scss";
+import "....../assets/stylesheets/bootstrap_custom.scss";
+
+Rails.start();
