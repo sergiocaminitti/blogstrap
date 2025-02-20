54b444da0d1971a6e01c44f350abf432747230b8
SergioCaminitti <sergiocaminitti@gmail.com>
2025-02-20 16:07:41 -0300

    add feature destroy comment

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -2,4 +2,7 @@ class CommentPolicy < ApplicationPolicy
   def create?
     user&.id
   end
+  def destroy?
+    user&.id == record.user_id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
 
-
-
   resources :articles do
-    resources :comments, only: %i[create]
+    resources :comments, only: %i[create destroy]
   end
-
-
 
   resources :categories, except: [:show]
 end
